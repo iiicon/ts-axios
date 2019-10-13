05562fcaa1f732bf90b47e3b4224916720b317b6
shiguangwei <shigw5420@joyowo.com>
2019-10-13 18:06:54 +0800

    feat(examples 编写): 实现webpack.config和server，编写简单的demo
    多入口文件，实现最简单的请求

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'OPTIONS'
+  | 'options'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
