3808c176ff9658624bf893cc7a17bd0f1aef5d4b
shiguangwei <shigw5420@joyowo.com>
2019-11-04 08:28:37 +0800

    feat(processConfig): 处理请求url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig) {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig) {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
