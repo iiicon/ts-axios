cb90bb54ca396da855c199c4400166635f78c390
shiguangwei <shigw5420@joyowo.com>
2019-12-04 00:09:45 +0800

    使用 Jest 开始单元测试

---
+++ test/helpers/util.spec.ts
@@ -0,0 +1,32 @@
+import {
+  isDate,
+  isPlainObject,
+  isFormData,
+  isURLSearchParams,
+  extend,
+  deepMerge
+} from '../../src/helpers/util'
+
+describe('helpers:util', () => {
+  describe('isXX', () => {
+    test('should validate Date', () => {
+      expect(isDate(new Date())).toBeTruthy()
+      expect(isDate(Date.now())).toBeFalsy()
+    })
+
+    test('should validate PlainObject', () => {
+      expect(isPlainObject({})).toBeTruthy()
+      expect(isPlainObject(123)).toBeFalsy()
+    })
+
+    test('should validate FormData', () => {
+      expect(isFormData(new FormData())).toBeTruthy()
+      expect(isFormData({})).toBeFalsy()
+    })
+
+    test('should validate URLSearchParams', () => {
+      expect(isURLSearchParams(new URLSearchParams())).toBeTruthy()
+      expect(isURLSearchParams(123)).toBeFalsy()
+    })
+  })
+})
