47bae6b3468dfe71249778bb5bb47c4553970404
shiguangwei <shigw5420@joyowo.com>
2019-12-08 23:36:17 +0800

    添加测试代码

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -51,7 +51,7 @@ export function isFormData(val: any): val is FormData {
 }
 
 export function isAbsoluteURL(url: string): boolean {
-  return /([a-z][a-z\d\+\-\.]*:)\/\//i.test(url)
+  return /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url)
 }
 
 export function combineURL(baseURL: string, relativeURL?: string): string {
---
+++ test/boot.ts
@@ -0,0 +1,8 @@
+require('jasmine')
+
+const JasmineCore = require('jasmine-core')
+// @ts-ignore
+global.getJasmineRequireObj = function() {
+  return JasmineCore
+}
+require('jasmine-ajax')
